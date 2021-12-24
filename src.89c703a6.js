5eebb2c829cd5497d8ffe17c67775d342ae169da
VasylArtymovych <92371486+VasylArtymovych@users.noreply.github.com>
2021-12-24 19:43:03 +0000

    Deploying to gh-pages from @ riko1212/45-test-project@b5870dd7724ebf73756615d3251827a6677993e3 🚀

--- src.89c703a6.js
+++ src.89c703a6.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/45-test-project/src.473cb3c7.js.map
+//# sourceMappingURL=/45-test-project/src.89c703a6.js.map
